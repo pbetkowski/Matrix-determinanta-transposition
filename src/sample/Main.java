6413eea07cf1a0773895f65685824c0dbaa67baa
Przemyslaw Betkowski <przbetkowski@gmail.com>
2018-02-28 18:10:13 +0100

    Last fixes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("xml/sample.fxml"));
-        primaryStage.setTitle("Wyznacznik macierzy 2, 3 i 4 stopnia.");
+        primaryStage.setTitle("Operacja na macierzach.");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
         primaryStage.setResizable(false);
